98908babd81c00cfadbde339acf097776072bc95
teffalump <timelessmomentum@gmail.com>
2011-12-20 19:04:56 -0800

    calculate correct vertices and print them out; so that is mostly done, now just need to figure out if this is the best way to do this, lol

--- scad-construction.py
+++ scad-construction.py
@@ -39,7 +39,7 @@ def offsets_to_points(points, offsets=[[.8, 0], [.2, 1]]):
 def bezierSplinePoints(points, steps=100, offsets=[[.8, 0], [.2, 1]]):
     #points = cubic bezier spline must go through these
     #steps = number of steps per bez line
-    #offsets = control po)nt offsets relative to the diff between p0 and p3
+    #offsets = control point offsets relative to the diff between p0 and p3
     #that is, [.5, .5] would mean p1 would be halfway between p0[0] and p3[0], and halfway between p0[1] and p3[1]
 
     #return points to fill in the full line (excluding original points)
@@ -50,22 +50,48 @@ def bezierSplinePoints(points, steps=100, offsets=[[.8, 0], [.2, 1]]):
 
 
 from sys import exit
-test_data=random_data(100,50)
-steps=100
+test_data=random_data(2,3)
+steps=10
 
-from pprint import pprint
-for sl in range(len(test_data) - 1):
-    v_vert = []
-    h_vert = []
-    for i,c in enumerate(test_data[sl]):
-        c_points = offsets_to_points([[0,c], [1,test_data[sl+1][i]]])
-        v_points = [pointAlongBez4([0,c], c_points[0], c_points[1], [1,test_data[sl+1][i]], j/steps) for j in range(steps+1)]
-        v_vert.append([[i, x[0]+sl, x[1]] for x in v_points])
+with open('/home/cz/Programming/specSculpt/test.scad', 'w') as out:
+    out.write("polyhedron ( points = [")
+    from pprint import pprint
+    for sl in range(len(test_data) - 1):
+        v_vert = []
+        h = []
+        for i,c in enumerate(test_data[sl]):
+            print("calculating c_points")
+            c_points = offsets_to_points([[0,c], [1,test_data[sl+1][i]]])
+            print("getting vertical vertices")
+            v_points = [pointAlongBez4([0,c], c_points[0], c_points[1], [1,test_data[sl+1][i]], j/steps) for j in range(steps+1)]
+            print("done getting v vertices")
 
-    #need numpy
-    v = np.array(v_vert)
-    for t in range(len(v)):
-        a=bezierSplinePoints(v[:,t])
+            # index is x-value
+            v_vert.append([np.insert(x, 0, i) for x in v_points])
+
+        #the vertical slice are now in (x,y,z) coordinates...i think
+        v = np.around(v_vert,2)
+        pprint(v)
+        print("getting slice vertices")
+        for t in range(steps+1):
+
+            #these are all the transverse slices
+            #same y-value, so need:
+            #    slice horizontally through the vertical vertices
+            #    slice 0 and 2 index for each vertice point (i.e., only x and z, since all same y-value)
+            #    push that to bezierSplinePoints function
+            #    reinsert y-value along vert axis
+
+            h.append(np.insert(list(bezierSplinePoints(np.squeeze(v[...][:,t,0:3:2]))), 1, v[0][t][1], axis=1).tolist())
+            print("done with a slice")
+
+        pprint(np.array(h))
+        for s in np.array(h):
+            for point in s:
+                out.write("[")
+                point.tofile(out,",")
+                out.write("],\n")
+    out.write("[]]);")
 
         
 #v_vert now has all vertical vertices (x,y,z)...hopefully
